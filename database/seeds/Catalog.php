ab98357a138de318b5d6367e538be01285b2d61f
AKh <konopkin.a.wezom@gmail.com>
2019-01-24 21:45:54 +0200

    Sync

--- database/seeds/Catalog.php
+++ database/seeds/Catalog.php
@@ -16,7 +16,7 @@ class Catalog extends Seeder
 
     public function run()
     {
-        DB::table($this->groupsTable)->delete();
+        //DB::table($this->groupsTable)->delete();
 
         $groups = [
             [
@@ -106,20 +106,20 @@ class Catalog extends Seeder
             ],
         ];
 
-        foreach ($groups as $group) {
-            DB::table($this->groupsTable)->insert([
-                'id' => $group['id'],
-                'parent_id' => $group['parent_id'],
-                'status' => true,
-                'alias' => $group['alias'],
-                'name' => $group['name'],
-                'h1' => $group['name'],
-                'text' => $group['text'],
-                'image' => null,
-                'created_at' => new DateTime,
-                'updated_at' => new DateTime,
-            ]);
-        }
+        //foreach ($groups as $group) {
+        //    DB::table($this->groupsTable)->insert([
+        //        'id' => $group['id'],
+        //        'parent_id' => $group['parent_id'],
+        //        'status' => true,
+        //        'alias' => $group['alias'],
+        //        'name' => $group['name'],
+        //        'h1' => $group['name'],
+        //        'text' => $group['text'],
+        //        'image' => null,
+        //        'created_at' => new DateTime,
+        //        'updated_at' => new DateTime,
+        //    ]);
+        //}
         $itemsCount = 100;
         $faker = Faker::create();
 
@@ -134,7 +134,7 @@ class Catalog extends Seeder
         // Items
         $otherItemsNames = ['Духи', 'Шампунь', 'Гель для душа', 'Мыло', 'Дезодорант', 'Косметика', 'Футболка', 'Солнцезащитные очки', 'Купальник', 'Блуза', 'Рубашка', 'Брюки', 'Бриджи', 'Лосины', 'Джинсы', 'Шорты', 'Юбка', 'Комбинезон', 'Джемпер', 'Свитер', 'Гольф', 'Пуловер', 'Кофта', 'Кардиган', 'Жакет', 'Пиджак', 'Болеро', 'Топ', 'Футболка', 'Майка', 'Поло', 'Футболка с длинным рукавом', 'Тенниска', 'Реглан', 'Платье', 'Сарафан', 'Туника', 'Жилет', 'Пончо', 'Ветровка', 'Куртка', 'Пальто', 'Плащ', 'Полушубок', 'Пуховик', 'Спортивный костюм'];
 
-        for ($i = 0 ; $i <= 100 ; $i++) {
+        for ($i = 0 ; $i <= 10000 ; $i++) {
             $groups = array_filter($groups, function ($el) {
                 return isset($el['autogenerate_items_name']);
             });
@@ -150,18 +150,20 @@ class Catalog extends Seeder
                 $alias = trim(preg_replace('/[^a-z^\-]/', '', preg_replace('/\s/', '-', mb_strtolower($itemName))), '-');
             }
 
-            DB::table($this->itemsTable)->insert([
-                'group_id'   => $group['id'],
-                'status'     => true,
-                'alias'      => $alias,
-                'name'       => $itemName,
-                'h1'         => $itemName,
-                'text'       => $faker->realText(),
-                'image'      => null,
-                'cost'       => $faker->randomFloat(2, 100, 10000),
-                'created_at' => new DateTime,
-                'updated_at' => new DateTime,
-            ]);
+            try {
+                DB::table($this->itemsTable)->insert([
+                    'group_id' => $group['id'],
+                    'status' => true,
+                    'alias' => $alias,
+                    'name' => 'z' . $itemName,
+                    'h1' => $itemName,
+                    'text' => $faker->realText(),
+                    'image' => null,
+                    'cost' => $faker->randomFloat(2, 100, 10000),
+                    'created_at' => new DateTime,
+                    'updated_at' => new DateTime,
+                ]);
+            } catch (Exception $e) {}
         }
     }
 }
